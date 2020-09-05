c1f21f997bf201ad6022e9f7b6bc11420a6a3d46
miiiiiYT <pika.finn10@gmail.com>
2020-09-05 17:37:57 +0200

    Refined the functions and added descriptions

--- Crypt.py
+++ Crypt.py
@@ -1,53 +1,36 @@
-
 from Crypto.Cipher import AES
 import hashlib
 
-
-class Unverified(Exception):
-    pass
-
-
-class NotProperlyDefined(Exception):
-    pass
-
 def encrypt(password, message):
     """Encrypt a message using AES, returns a dictionary"""
-    try:
-        sha = hashlib.sha256(password.encode())
-        key = sha.digest()
-        messec = message.encode()
-        out = dict()
-        ci = AES.new(key, AES.MODE_EAX)
-        nonce = ci.nonce
-        citext, tag = ci.encrypt_and_digest(messec)
-        out['key'] = key
-        out['ciphertext'] = citext
-        out['tag'] = tag
-        out['nonce'] = nonce
-        return out
 
-    except AttributeError:
-        raise ValueError('Arguments of encrypt() must be strings')
-
-
-def decrypt(enc_dict):
-    if isinstance(enc_dict, dict):
-        try:
-            ci = AES.new(enc_dict['key'], AES.MODE_EAX, nonce=enc_dict['nonce'])
-            plain = ci.decrypt(enc_dict['ciphertext'])
-            ci.verify(enc_dict['tag'])
-            return plain
-        except ValueError:
-            raise Unverified('Key incorrect or message corrupted.')
-            
-    else:
-        raise(ValueError('Please use a dictionary as argument'))
+    # Create a new SHA256 object initialized with the given password
+    sha = hashlib.sha256(password.encode())
+    # Saves the digest of the password as the key
+    key = sha.digest()
+    # Encodes the given message
+    messec = message.encode()
+    # Creates output dictionary
+    out = dict()
+    # Creates an AES object and saves it's nonce
+    ci = AES.new(key, AES.MODE_EAX)
+    nonce = ci.nonce
+    # Encrypts the message
+    citext, tag = ci.encrypt_and_digest(messec)
+    # Saves everything in the output dict and returns it.
+    out['ciphertext'] = citext
+    out['tag'] = tag
+    out['nonce'] = nonce
+    return out
 
 
-print('Encrypted Data:')
-enc = encrypt('1234', 'Uhu')
-print(enc)
-print('')
-
-print('Decrypted Data:\n')
-plain = decrypt(enc)
+def decrypt(enc_dict, password):
+    """Decrypts a message using AES, needs an dictionary containing ciphertext, nonce and tag, also uses a password."""
+    # Creates Hash of the password
+    sha = hashlib.sha256(password.encode())
+    # Creates AES object
+    ci = AES.new(key=sha.digest(), mode=AES.MODE_EAX, nonce=enc_dict['nonce'])
+    # Decrypts, verifies and returns the ciphertext.
+    plain = ci.decrypt(enc_dict['ciphertext'])
+    ci.verify(enc_dict['tag'])
+    return plain
