1a4842a7bacfc6496acbb2040eea4cdd6dbeea30
miiiiiYT <41822967+miiiiiYT@users.noreply.github.com>
2021-02-04 12:50:37 +0100

    Made this repo into a PyPI package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='aes-crypter',
+      version='1.0',
+      description='A wrapper for AES encryption using PyCryptoDome',
+      url='https://github.com/miiiiiYT/aes-crypter',
+      author='miiiiiYT',
+      author_email='41822967+miiiiiYT@users.noreply.github.com',
+      license='MIT',
+      packages=['aes-crypter'],
+      zip_safe=False)
